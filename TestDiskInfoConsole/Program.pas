e74316dc1ddeab1796a96da0befb81d3b792fe9a
corneliusdavid <dev@corneliusconcepts.com>
2015-01-05 05:29:23 -0800

    console app to test the DiskInfo library

---
+++ TestDiskInfoConsole/Program.pas
@@ -0,0 +1,38 @@
+﻿namespace TestDiskInfoConsole;
+
+interface
+
+uses
+  System.Linq;
+
+type
+  ConsoleApp = class
+  public
+    class method Main(args: array of String);
+  end;
+
+implementation
+
+uses
+  DiskInfoLib;
+
+class method ConsoleApp.Main(args: array of String);
+var
+  di: DiskInfo := new DiskInfo;
+begin
+  Console.WriteLine('DiskInfo tests...');
+  Console.WriteLine;
+
+  Console.WriteLine('Current disk: ' + di.CurrentDiskName);
+  Console.WriteLine('Disk label: ' + di.VolumeName);
+  Console.WriteLine('Disk type: ' + di.DiskType);
+  Console.WriteLine('Disk size: ' + di.TotalDiskSize.ToString + ' or ' + (di.TotalDiskSize / 1024 / 1024).ToString + ' MB');
+  Console.WriteLine('Disk free: ' + di.TotalDiskFree.ToString + ' or ' + (di.TotalDiskFree / 1024 / 1024).ToString + ' MB');
+
+  Console.WriteLine;
+  Console.Write('Press ENTER...');
+
+  Console.ReadLine;
+end;
+
+end.
